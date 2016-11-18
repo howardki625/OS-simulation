b3b7516a63c57bdaf8df075225d14cc5c5eeecd9
howardki625 <howardki625@gmail.com>
2016-11-17 22:29:58 -0500

    Add files via upload

---
+++ sos/os.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+// channel commands from the simulator.
+void siodisk(long jobnum);
+void siodrum(long jobnum, long jobsize, long coreaddress, long direction);
+    // direction: 1 - core to drum, 0 - drum to core.
+
+// for debugging use.
+void ontrace();
+void offtrace();
+
+void startup()
+{
+    // initializes the system.
+    // called once at startup.
+}
+
+//--------------------//
+// interrupt handlers //
+//--------------------//
+
+void Crint(long &a, long p[])
+{
+    // indicates arrival of new job on drum.
+}
+
+void Dskint(long &a, long p[])
+{
+    // disk interrupt.
+}
+
+void Drmint(long &a, long p[])
+{
+    // drum interrupt.
+}
+
+void Tro(long &a, long p[])
+{
+    // timer-run-out.
+}
+
+void Svc(long &a, long p[])
+{
+    // user program supervisor call.
+}
+
+//--------------------------//
+// any additional functions //
+//--------------------------//

cd44e67ac35100d6634e9b7f144f05f163f01766
howardki625 <howardki625@gmail.com>
2016-12-03 21:28:41 -0500

    Add files via upload

--- sos/os.cpp
+++ sos/os.cpp
@@ -7,8 +7,10 @@ using namespace std;
 
 // global variables
 vector<job> joblist; // a vector to store jobs coming from sos.
+long runningJob; // variable indicating the location of the running job on the vector.
 
 // function prototypes
+void bookkeeper(long); // book keeping function - keeps track of time.
 
 // channel commands from the simulator.
 void siodisk(long jobnum);
@@ -23,6 +25,8 @@ void startup()
 {
     // initializes the system.
     // called once at startup.
+    ontrace();
+    runningJob = -1; // initialize at -1 since joblist[0] can have a job in it.
 }
 
 //--------------------//
@@ -32,6 +36,8 @@ void startup()
 void Crint(long &a, long p[])
 {
     // indicates arrival of new job on drum.
+
+    bookkeeper(p[5]); // updates the time.
 
     // push job onto joblist
     joblist.push_back(job(p[1], p[2], p[3], p[4], p[5]));
@@ -60,3 +66,16 @@ void Svc(long &a, long p[])
 //--------------------------//
 // any additional functions //
 //--------------------------//
+
+void bookkeeper(long time)
+{
+    // keeps track of time for the currently running job
+    // called before every interrupt to update the timer.
+    if (runningJob != -1)
+    {
+        // updates the CPU used time using the current time and the time the job entered the system.
+        long t;
+        t = time - joblist[runningJob].getEntryTime();
+        joblist[runningJob].setCpuUsedTime(t);
+    }
+}
